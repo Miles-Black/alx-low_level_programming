389d3603a8b266830e7e86659cfa322d07ae96a4
Miles-black <sangmendonmaaye@gmail.com>
2022-09-13 17:54:36 -0700

    done1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,15 @@
  *
  * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
-	char c;
-	char i;
-	
+	char c, i;
+
 	for (i = 0; i <= 9; i++)
 	{
-	for (c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
-	_putchar(c);
+			_putchar(c);
 		}
 		_putchar('\n');
 	}
