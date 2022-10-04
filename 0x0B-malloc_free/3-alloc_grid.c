dd8ea8609a58842ad247ccf3097e8ccbffbf38b4
Miles-black <sangmendonmaaye@gmail.com>
2022-10-04 04:38:53 -0700

    tsk3error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,8 +15,10 @@ int **alloc_grid(int  width, int height)
 	int i = 0, j;
 
 	if (width = 0 || height == 0)
+	{
 		return (NULL);
 	array = (int **) malloc(sizeof(int *) * height);
+	}
 	if (array != NULL)
 	{
 		for (; i < height; i++)
